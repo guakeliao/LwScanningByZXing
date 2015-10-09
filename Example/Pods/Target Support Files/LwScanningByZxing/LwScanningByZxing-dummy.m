3e50ec63e5a093805623cc2804b01829025ae77b
liaowei <598951856@qq.com>
2015-10-09 16:12:21 +0800

    工程初始化

---
+++ Example/Pods/Target Support Files/LwScanningByZxing/LwScanningByZxing-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LwScanningByZxing : NSObject
+@end
+@implementation PodsDummy_LwScanningByZxing
+@end
---
+++ Example/Pods/Target Support Files/LwScanningByZxing/LwScanningByZxing-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LwScanningByZxingVersionNumber;
+FOUNDATION_EXPORT const unsigned char LwScanningByZxingVersionString[];
+
